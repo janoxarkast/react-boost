23dd3272370b1194d9bc9e8658f007a08e6d11a0
gianni.toniolo <gianni.toniolo@alten.it>
2021-11-13 20:43:12 +0100

    styling

--- src/components/Views/CreatureDetail/index.js
+++ src/components/Views/CreatureDetail/index.js
@@ -1,19 +1,19 @@
-import {Card, Col, Container, Row, Spinner} from "react-bootstrap";
+import {Badge, Card, Col, Container, ListGroup, Row, Spinner} from "react-bootstrap";
 import {useApi} from "../../../Hooks/useApi";
-import { useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
+import "./creature-detail.css"
 
 
 const CreatureDetail = () => {
-    let { id } = useParams();
-    const { data: creature, loading } = useApi(`https://botw-compendium.herokuapp.com/api/v2/entry/${id}`)
+    let {id} = useParams();
+    const {data: creature, loading} = useApi(`https://botw-compendium.herokuapp.com/api/v2/entry/${id}`)
 
     return (
         <>
-
             <Container>
-                { loading && <Spinner variant={'light'} animation="border" role="status">
+                {loading && <Spinner variant={'light'} animation="border" role="status">
                     <span className="visually-hidden">Loading...</span>
-                </Spinner> }
+                </Spinner>}
                 {creature && creature.data &&
                 <>
                     <Row className={'my-5'}>
@@ -21,23 +21,53 @@ const CreatureDetail = () => {
                     </Row>
                     <Row xs={1} className="g-4">
                         <Col>
-                            <Card bg={'dark'} text={'white'} border={'warning'} >
-                                <Card.Header>
-                                    <Card.Title className={'text-capitalize'}>{creature.data.name}</Card.Title></Card.Header>
-                                <Card.Img variant="top"
-                                          src={`https://botw-compendium.herokuapp.com/api/v2/entry/${creature.data.name}/image`}/>
-                                <Card.Body>
-                                    <Card.Text>
+                            <article className="postcard dark red">
+                                <a className="postcard__img_link" href="#">
+                                    <img className="postcard__img"
+                                         src={`https://botw-compendium.herokuapp.com/api/v2/entry/${creature.data.name}/image`}
+                                         alt={creature.data.name}/>
+                                </a>
+                                <div className="postcard__text">
+                                    <h1 className="postcard__title red"><a href="#">{creature.data.name}</a></h1>
+                                    <div className="postcard__subtitle small text-warning">
+                                        {creature.data.category}
+                                    </div>
+                                    <div className="postcard__bar"></div>
+                                    <div className="postcard__preview-txt">
                                         {creature.data.description}
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
+                                    </div>
+                                </div>
+                            </article>
+
+                            <article className={'postcard dark red'}>
+                                <div className="postcard__text">
+                                    <Row className={'mt-3'}>
+                                        <Col>
+                                            <h4 className="postcard__title red text-center">Locations</h4>
+                                            <ul className="postcard__tagbox">
+                                                {creature.data.common_locations.length > 0 &&
+                                                creature.data.common_locations.map((loc, index) => (
+                                                    <li key={index} className="tag__item">{loc}</li>))}
+                                            </ul>
+                                        </Col>
+                                        <Col>
+                                            <h4 className="postcard__title red text-center">Drops</h4>
+                                            <ListGroup>
+                                                {
+                                                creature.data.drops.length > 0 &&
+                                                creature.data.drops.map((drop, index) => (
+                                                    <ListGroup.Item key={index} action variant="dark">{drop}</ListGroup.Item>))
+                                                }
+                                            </ListGroup>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </article>
                         </Col>
                     </Row>
                 </>
                 }
             </Container>
-
         </>
 
     )
--- src/components/Views/Creatures/index.js
+++ src/components/Views/Creatures/index.js
@@ -1,10 +1,11 @@
 import {Button, Card, Col, Container, Row, Spinner} from "react-bootstrap";
 import {useApi} from "../../../Hooks/useApi";
 import {Link} from "react-router-dom";
+import './creatures.css'
 
 
 const Creatures = () => {
-    const { data: creatures, loading } = useApi('https://botw-compendium.herokuapp.com/api/v2/category/monsters')
+    const {data: creatures, loading} = useApi('https://botw-compendium.herokuapp.com/api/v2/category/monsters')
 
     return (
         <>
@@ -12,31 +13,37 @@ const Creatures = () => {
                 <Row className={'my-5'}>
                     <h1 className={'text-white'}>Creatures Compendium</h1>
                 </Row>
-                { loading && <Spinner variant={'light'} animation="border" role="status">
+                {loading && <Spinner variant={'light'} animation="border" role="status">
                     <span className="visually-hidden">Loading...</span>
-                </Spinner> }
+                </Spinner>}
                 <Row xs={2} md={3} lg={3} className="g-4">
                     {creatures && creatures.data && creatures.data.map((_) => (
                         <Col key={_.id}>
-                            <Card bg={'dark'} text={'white'} border={'warning'} >
-                                <Card.Header>
-                                    <Card.Title className={'text-capitalize'}>{_.name}</Card.Title></Card.Header>
-                                <Card.Img variant="top"
-                                          src={`https://botw-compendium.herokuapp.com/api/v2/entry/${_.name}/image`}/>
-                                <Card.Body>
-                                    <Card.Text>
-                                        {_.description.substring(0, 90) + '...'}
-                                    </Card.Text>
-                                    <Button>
-                                        <Link to={`creature/${_.id}`}> click me</Link>
-                                    </Button>
-                                </Card.Body>
+                            <Card className={'text-white card-has-bg click-col'}
+                                  style={{backgroundImage: `url("https://botw-compendium.herokuapp.com/api/v2/entry/${_.name}/image")`}}>
+                                <img className="card-img d-none"
+                                     src={`https://botw-compendium.herokuapp.com/api/v2/entry/${_.name}/image`}/>
+                                <Card.ImgOverlay className={'card-img-overlay d-flex flex-column'}>
+                                    <Card.Body>
+                                        <small className="card-meta mb-2">{_.category}</small>
+                                        <h4 className="card-title mt-0 "><Link className="text-white"
+                                                                               to={`creature/${_.id}`}> {_.name}</Link>
+                                        </h4>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <div className="media">
+                                            <Button variant={'outline-light'}>
+                                                Dettaglio
+                                            </Button>
+                                        </div>
+
+                                    </Card.Footer>
+                                </Card.ImgOverlay>
                             </Card>
                         </Col>
                     ))}
                 </Row>
             </Container>
-
         </>
 
     )
